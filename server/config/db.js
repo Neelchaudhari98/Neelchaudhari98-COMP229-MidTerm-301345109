7f45395b7c0c5419adca6df5d308fd0458151ec8
Neelchaudhari98 <neelchaudhari98@gmail.com>
2023-03-03 14:47:02 -0500

    Mid term final commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books"
+  "URI": "mongodb+srv://admin:admin@books.ixwuzap.mongodb.net/?retryWrites=true&w=majority"
+};
